771836d447a73d5bf6c2665ee8c5ecee29fc3aab
JanithIsu <kumarawkjanithisuru@gmail.com>
2025-04-25 00:15:52 +0530

    Create GUI
    Janith

---
+++ Technical officer/src/Update_Profile.java
@@ -0,0 +1,12 @@
+import javax.swing.*;
+
+public class Update_Profile {
+    private JPanel panel1;
+    private JTextField textField1;
+    private JTextField textField3;
+    private JTextField textField4;
+    private JTextField textField5;
+    private JButton updateCancelButton;
+    private JButton cancelButton;
+    private JTextArea textArea1;
+}
---
+++ Technical officer/src/Viwe_Attendance.java
@@ -0,0 +1,8 @@
+import javax.swing.*;
+
+public class Viwe_Attendance {
+    private JPanel panel1;
+    private JButton updateButton;
+    private JButton addButton;
+    private JTable table1;
+}
