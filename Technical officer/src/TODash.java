f47804e65c71bd9ccd92d73a62ff22794125bb23
JanithIsu <kumarawkjanithisuru@gmail.com>
2025-04-24 20:16:36 +0530

    commited

---
+++ Technical officer/src/TODash.java
@@ -0,0 +1,24 @@
+import javax.swing.*;
+
+public class TODash {
+    private JPanel mainPanel;
+    private JPanel headingPanel;
+    private JPanel buttonPanel;
+    private JPanel contentPanel;
+    private JButton profileButton;
+    private JButton timeTableButton;
+    private JButton medicalButton;
+    private JButton attendanceButton;
+    private JTextField textField1;
+    private JTextField textField2;
+    private JButton uploadButton;
+    private JTextField textField3;
+    private JTextField textField4;
+    private JTextField textField5;
+    private JTextField textField6;
+    private JTextField textField7;
+
+    private void createUIComponents() {
+        // TODO: place custom component creation code here
+    }
+}
